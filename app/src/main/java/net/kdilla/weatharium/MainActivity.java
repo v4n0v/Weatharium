a47f606fd0c5d3998852017dd2ec05b94f560bd4
v4n0v <avetchinov@gmail.com>
2017-10-30 11:39:36 +0300

    русификация работает, при смене языка все ок
        сохраняет настройки погоды для выбранной страны
        при новом запуске выводит последнюю сохранную
        сохраняет настройки при выходе из приложения

--- app/src/main/java/net/kdilla/weatharium/MainActivity.java
+++ app/src/main/java/net/kdilla/weatharium/MainActivity.java
@@ -1,13 +1,73 @@
 package net.kdilla.weatharium;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnRun;
+    private Spinner spinnerSelectCountry;
+    private SharedPreferences preferences;
+    private TextView textViewWeather;
+    private final String SAVED_COUNTRY = "saved countery";
+    private String currentWeather;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        loadPreferencesAndShowWeather();
+
+    }
+
+    private void initViews() {
+        spinnerSelectCountry = (Spinner) findViewById(R.id.spinner_sel_country);
+        btnRun = (Button) findViewById(R.id.btn_run);
+        btnRun.setOnClickListener(onClickListener);
+        textViewWeather = (TextView) findViewById(R.id.textview_weather);
+    }
+
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.btn_run) {
+                savePreferences();
+                showWeather(currentWeather);
+            }
+        }
+    };
+
+    void showWeather(String weather){
+        textViewWeather.setText(weather);
+    }
+    private void savePreferences() {
+        preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor ed = preferences.edit();
+        currentWeather = WeatherByCountry.getWeatherInCountry(MainActivity.this, spinnerSelectCountry.getSelectedItemPosition());
+        ed.putString(SAVED_COUNTRY, currentWeather);
+        ed.commit();
+
+    }
+
+    private void loadPreferencesAndShowWeather() {
+        preferences = getPreferences(MODE_PRIVATE);
+        String savedPrefs = preferences.getString(SAVED_COUNTRY, "");
+        textViewWeather.setText(savedPrefs);
+        Toast.makeText(MainActivity.this, (R.string.country_loaded), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        savePreferences();
     }
 }
+
---
+++ app/src/main/java/net/kdilla/weatharium/WeatherByCountry.java
@@ -0,0 +1,25 @@
+package net.kdilla.weatharium;
+
+import android.content.Context;
+
+/**
+ * Created by avetc on 30.10.2017.
+ */
+
+class WeatherByCountry {
+   public String[] countriesList;
+
+    static  String getWeatherInCountry(Context context, int pos){
+        String[] countriesWeatherList = context.getResources().getStringArray(R.array.weather);
+
+        return countriesWeatherList[pos];
+    }
+
+
+    static String[] getCountriesList(Context context){
+        String[] countriesList = context.getResources().getStringArray(R.array.country_selection);
+
+        return countriesList;
+    }
+
+}
